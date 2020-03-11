c40ea9e551f1b1a3e06a41d551d0657583f481a8
Extacy <43688161+Extacy@users.noreply.github.com>
2020-03-11 21:09:49 +1300

    add workshop_collection_sync.sh

---
+++ scripts/workshop_collection_sync.sh
@@ -0,0 +1,82 @@
+# https://github.com/Extacy/misc/scripts/workshop_collection_sync.sh
+
+collections=(1942732975 1942753106 1942767730 1942784132 1942796140)
+
+# Loop over collections and grab each map's ID
+ids=()
+for id in ${collections[@]}; do
+    collection=$(curl --silent -H "Content-Type: application/x-www-form-urlencoded" \
+        -d "collectioncount=1" \
+        -d "publishedfileids[0]=$id" \
+        https://api.steampowered.com/ISteamRemoteStorage/GetCollectionDetails/v1/)
+
+    keys=$( echo "$collection" | jq -r ".response | .collectiondetails[0] | .children | keys | .[]")
+
+    for key in $keys; do
+        item=$( echo "$collection" | jq -r ".response | .collectiondetails[0] | .children | nth($key) | .publishedfileid")
+        ids+=($item)
+    done
+done
+
+# Grab a list of maps that are on the server
+maps=()
+for dir in $(find -type d); do
+    map=$( echo $dir | sed -e "s%./%%")
+
+    if [[ $map =~ [[:digit:]]{3,} ]]; then
+        maps+=($map)
+    fi
+done
+
+# Loop over workshop items, comparing versions and downloading if needed.
+# Subtract from maps=() and then loop over what's remaining and remove that.
+for id in ${ids[@]}; do
+    response=$(curl --silent -H "Content-Type: application/x-www-form-urlencoded" \
+        -d "itemcount=1" \
+        -d "publishedfileids[0]=$id" \
+        https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/)
+
+    name=$( echo $response | jq -r ".response | .publishedfiledetails[0] | .filename" | sed -e "s%mymaps/%%")
+    size=$( echo $response | jq -r ".response | .publishedfiledetails[0] | .file_size")
+    url=$( echo $response | jq -r ".response | .publishedfiledetails[0] | .file_url")
+
+    if [ ! -d "$id" ] || [ ! -f "$id/$name" ]; then
+        # Does not exist, download.
+        mkdir -p $id
+        wget -q --show-progress $url -O $id/$name
+    else
+        # Exists, compare size & download if differ
+        if [ $size != $(stat --printf="%s" $id/$name) ]; then
+            wget -q --show-progress $url -O $id/$name
+        fi
+    fi
+    maps=(${maps[@]/$id})
+done
+
+# Maps that are downloaded, but don't exist in the collection anymore.
+for map in ${maps[@]}; do
+    rm -r $map
+done
+
+
+# MIT License
+# 
+# Copyright (c) 2020 Extacy
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
